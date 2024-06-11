e722c8f7372c8629a636ae4ae8677cb1a73af8b1
Xiran Jia <jiaxiran98@gmail.com>
2024-06-10 20:59:43 -0700

    3 done
    Change-Id: I4ee5e79cbb7f188d761808fde58e1f23a2b33399

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import express from 'express'
+import "dotenv/config";
+import mongoose from "mongoose";
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js"
 import CourseRoutes from './Kanbas/Courses/routes.js'
 import cors from "cors"
 import ModuleRoutes from './Kanbas/Modules/routes.js'
 import AssignmentRoutes from './Kanbas/Assignments/routes.js'
+import UserRoutes from './Users/routes.js';
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://localhost:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
 app.use(express.json());
 Hello(app)
+UserRoutes(app)
 AssignmentRoutes(app)
 ModuleRoutes(app)
 CourseRoutes(app)
